a4dd7196ee1492ee79f5d3cce9e6a9f25850da93
carlos-osorio-developer <osorio.uis@gmail.com>
2021-03-19 11:08:04 -0500

    Edit my_all to work without a block and with a Class as a parameter

--- enumerable.rb
+++ enumerable.rb
@@ -23,10 +23,18 @@ module Enumerable
     newarray
   end
 
-  def my_all?()
-    output = true
-    my_each { |item| output = false unless yield(item) }
-    output
+  def my_all?(arg=nil)
+    unless block_given?
+      if arg!=nil && arg.is_a?(Class)
+        my_each { |item| return false unless item.is_a?(arg) }
+        return true
+      else
+        self.my_each{|i| return false unless !!i}
+        return true
+      end
+    end    
+    my_each { |item| return false unless yield(item) }
+    return true
   end
 
   def my_any?()
@@ -68,4 +76,4 @@ module Enumerable
   end
 end
 
-p [1, 2, nil, 5].my_each()
+p [1, "j", 3.14].my_all?(Numeric)
